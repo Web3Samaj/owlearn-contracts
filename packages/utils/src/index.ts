37c6d7537dd3ee280026c8df4684244d90b0fd58
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-06-02 16:08:26 +0530

    feat: added method to check undefined values

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,1 +1,5 @@
-export const PACKAGE_NAME = "@owlearn/utils";
+import { checkForUndefined } from "./checkers/checkForUndefined";
+
+const PACKAGE_NAME = "@owlearn/utils";
+
+export {checkForUndefined, PACKAGE_NAME};
