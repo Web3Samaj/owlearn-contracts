e173c33b77c1d2aa03d63548b7011e946cbf4949
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-06-11 22:21:57 +0530

    featl: requested changes made in .gitmodules and OwlearnID

--- apps/contracts/src/OwlearnId.sol
+++ apps/contracts/src/OwlearnId.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
+// From https://github.com/Web3Samaj/owlearn-contracts/blob/3-feat-owlearn-id-for-users/apps/contracts/src/OwlearnId.sol
+
 import {OwlearnIdStorage} from "./storage/OwlearnIdStorage.sol";
 import {StringUtils} from "./libraries/StringUtils.sol";
 import {Base64} from "./libraries/Base64.sol";
@@ -22,6 +24,22 @@ contract OwlearnId is ERC721URIStorage, Ownable, OwlearnIdStorage {
 
     // domain extension ->  .owl
     string public tld;
+
+    uint256 public constant threeLetterMultipler = 10;
+    uint256 public constant fourLetterMultiplier = 5;
+    uint256 public constant priceMultiplier = 3;
+
+    /*///////////////////////////////////////////////////////////////
+                          Errors
+    //////////////////////////////////////////////////////////////*/
+    error Unauthorized();
+    error AlreadyRegistered();
+    error InvalidName(string name);
+
+    /*/////////////////////// NFT SVG  /////////////////////////////*/
+    string svgPartOne =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#B)" d="M0 0h270v270H0z"/><defs><filter id="A" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><path d="M72.863 42.949c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-10.081 6.032-6.85 3.934-10.081 6.032c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-8.013-4.721a4.52 4.52 0 0 1-1.589-1.616c-.384-.665-.594-1.418-.608-2.187v-9.31c-.013-.775.185-1.538.572-2.208a4.25 4.25 0 0 1 1.625-1.595l7.884-4.59c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v6.032l6.85-4.065v-6.032c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595L41.456 24.59c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-14.864 8.655a4.25 4.25 0 0 0-1.625 1.595c-.387.67-.585 1.434-.572 2.208v17.441c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l10.081-5.901 6.85-4.065 10.081-5.901c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v9.311c.013.775-.185 1.538-.572 2.208a4.25 4.25 0 0 1-1.625 1.595l-7.884 4.721c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-7.884-4.59a4.52 4.52 0 0 1-1.589-1.616c-.385-.665-.594-1.418-.608-2.187v-6.032l-6.85 4.065v6.032c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l14.864-8.655c.657-.394 1.204-.95 1.589-1.616s.594-1.418.609-2.187V55.538c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595l-14.993-8.786z" fill="#fff"/><defs><linearGradient id="B" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#cb5eee"/><stop offset="1" stop-color="#0cd7e4" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#A)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
+    string svgPartTwo = "</text></svg>";
 
     /**
      * @dev The constructor sets NFT Implementation and the TLD
@@ -47,11 +65,11 @@ contract OwlearnId is ERC721URIStorage, Ownable, OwlearnIdStorage {
         uint256 len = StringUtils.strlen(name);
         require(len > 0);
         if (len == 3) {
-            return 10 * 10 ** 15; // 10 Matic for len 3
+            return threeLetterMultipler * 1 ether; // 10 Matic for len 3
         } else if (len == 4) {
-            return 5 * 10 ** 15; // 5 Matic for len 4
+            return fourLetterMultiplier * 1 ether; // 5 Matic for len 4
         } else {
-            return 3 * 10 ** 15; // 3 Matic for len 5
+            return priceMultiplier * 1 ether; // 3 Matic for len 5
         }
     }
 
