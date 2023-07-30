483f7df77260e481e20cd7ba31fdc747af4af40d
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-07-30 20:31:38 +0200

    feat: reabse errors

--- apps/contracts/src/CourseFactory.sol
+++ apps/contracts/src/CourseFactory.sol
@@ -4,64 +4,192 @@ pragma solidity ^0.8.10;
 // Source : https://github.com/Web3Samaj/owlearn-contracts/blob/5-feat-coursefactory/apps/contracts/src/CourseFactory.sol
 
 import "./OwlearnCourse.sol";
-import "./OwlearnEducatorBadge.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-/// @title CourseFactory
+/// @title OwlearnCourseFactory
 /// @notice A Factory contract to create a new course with create2 method
 /// @author Dhruv <contact.dhruvagarwal@gmail.com>
-contract CourseFactory {
-    /*======================== Mappings & Variables  ========================*/
+contract OwlearnCourseFactory is Ownable {
+    /*///////////////////// Mappings & Variables //////////////////////////////////*/
 
     mapping(uint courseId => address courseCollectionAddress) public getCourse;
-    uint private _totalCourses = 1;
+    uint public totalCourses = 1;
     address[] public allCourses;
 
-    OwlearnEducatorBadge private _educatorBadgeNFT;
-
-    /*======================== Event ========================*/
-
-    event CourseCreated(address indexed courseAddress, address indexed creator);
-
-    /*======================== Constructor ========================*/
-
-    constructor(OwlearnEducatorBadge educatorBadgeNFT) {
-        _educatorBadgeNFT = educatorBadgeNFT;
+    struct CourseCreationProps {
+        string courseName;
+        string courseSymbol;
+        address courseCreator;
+        string courseURI;
+        string[] courseNFTURIs;
+        string certificateBaseURI;
     }
 
-    /*======================== Modifier Functions ========================*/
+    /*///////////////////// Events //////////////////////////////////*/
+    event CourseCreated(
+        address indexed courseAddress,
+        string courseName,
+        string courseSymbol,
+        address indexed creator,
+        string courseURI,
+        string[] courseNFTURIs,
+        string certificateBaseURI
+    );
 
-    modifier onlyEducatorBadgeHolder() {
-        require(
-            _educatorBadgeNFT.balanceOf(msg.sender, 1) == 1,
-            "ONLY EDUACATOR BADGE HOLDERS ALLOWED"
-        );
-        _;
-    }
+    constructor() public {}
 
-    // =============================================================
-    //                           EXTNERNAL FUNCTIONS
-    // =============================================================
+    /*///////////////////////////////////////////////////////////////
+                           External Functions
+    //////////////////////////////////////////////////////////////*/
     /**
      * @dev create a new course by deploying a Course Contract
+     *
+     * @param courseName -Name of the new course to be created
+     * @param courseSymbol -Name of the new course to be created
+     * @param courseURI  courseURI , containing any extra course Info
+     * @param courseNFTURIs  courseNFTURIs to be minted first, containing info about the particular resource
+     * @param certificateBaseURI -NFT URI , dynamic , off-chain server link , fetching progree & certificates for a Course Learner
      *
      * @return course address of the newly deployed contract
      * @return courseId Id of the course being deployed
      */
-    function createCourse()
-        external
-        onlyEducatorBadgeHolder
-        returns (address course, uint courseId)
-    {
-        bytes memory bytecode = type(OwlearnCourse).creationCode;
-        bytes32 salt = keccak256(abi.encodePacked(msg.sender, block.timestamp));
+    function createCourse(
+        string memory courseName,
+        string memory courseSymbol,
+        string memory courseURI,
+        string[] memory courseNFTURIs,
+        string memory certificateBaseURI
+    ) external returns (address course, uint courseId) {
+        bytes memory bytecode = _getByteCode(
+            courseName,
+            courseSymbol,
+            msg.sender,
+            courseURI,
+            courseNFTURIs,
+            certificateBaseURI
+        );
+        bytes32 salt = keccak256(abi.encodePacked(courseName, courseSymbol));
+        // OwlearnCourse _newCourse = new OwlearnCourse{salt: salt}(
+        //     courseName,
+        //     courseSymbol,
+        //     msg.sender,
+        //     courseURI,
+        //     courseNFTURIs,
+        //     certificateBaseURI
+        // );
+        OwlearnCourse _newCourse = _deployContract(bytecode, salt);
+
+        courseId = totalCourses;
+        getCourse[courseId] = address(_newCourse);
+        totalCourses += 1;
+        allCourses.push(address(_newCourse));
+        emit CourseCreated(
+            address(_newCourse),
+            courseName,
+            courseSymbol,
+            msg.sender,
+            courseURI,
+            courseNFTURIs,
+            certificateBaseURI
+        );
+    }
+
+    /**
+     * @dev get Address for the contract to be deployed
+     *
+     * @param courseName -Name of the new course to be created
+     * @param courseSymbol -Name of the new course to be created
+     * @param courseCreator  creator of the Course , who will also control the Collection
+     * @param courseURI  courseURI , containing any extra course Info
+     * @param courseNFTURIs  courseNFTURIs to be minted first, containing info about the particular resource
+     * @param certificateBaseURI -NFT URI , dynamic , off-chain server link , fetching progree & certificates for a Course Learner
+     *
+     * @return -  address of the new Course Contract to be deployed
+     */
+    function getAddress(
+        string memory courseName,
+        string memory courseSymbol,
+        address courseCreator,
+        string memory courseURI,
+        string[] memory courseNFTURIs,
+        string memory certificateBaseURI
+    ) public returns (address) {
+        bytes memory bytecode = _getByteCode(
+            courseName,
+            courseSymbol,
+            courseCreator,
+            courseURI,
+            courseNFTURIs,
+            certificateBaseURI
+        );
+        bytes32 salt = keccak256(abi.encodePacked(courseName, courseSymbol));
+
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                bytes1(0xff),
+                address(this),
+                salt,
+                keccak256(bytecode)
+            )
+        );
+
+        // NOTE: cast last 20 bytes of hash to address
+        return address(uint160(uint(hash)));
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                           Internal Functions
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @dev deploy the new Course Contract
+     *
+     * @param bytecode - bytecode of the new Course contract to be deployed
+     * @param salt - Salt for the new Course contract , to add the variation
+     *
+     * @return _newCourse -  the new OwlearnCourse Contract deployed
+     */
+    function _deployContract(
+        bytes memory bytecode,
+        bytes32 salt
+    ) internal returns (OwlearnCourse _newCourse) {
         assembly {
-            course := create2(0, add(bytecode, 32), mload(bytecode), salt)
+            _newCourse := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
-        // initialize with the inputs
-        courseId = _totalCourses;
-        getCourse[courseId] = course;
-        _totalCourses += 1;
-        allCourses.push(course);
-        emit CourseCreated(address(course), msg.sender);
+    }
+
+    /**
+     * @dev get ByteCode for the contract to be deployed
+     *
+     * @param courseName -Name of the new course to be created
+     * @param courseSymbol -Name of the new course to be created
+     * @param courseCreator  creator of the Course , who will also control the Collection
+     * @param courseURI  courseURI , containing any extra course Info
+     * @param courseNFTURIs  courseNFTURIs to be minted first, containing info about the particular resource
+     * @param certificateBaseURI -NFT URI , dynamic , off-chain server link , fetching progree & certificates for a Course Learner
+     *
+     * @return byteCode of the new Contract to be deployed
+     */
+    function _getByteCode(
+        string memory courseName,
+        string memory courseSymbol,
+        address courseCreator,
+        string memory courseURI,
+        string[] memory courseNFTURIs,
+        string memory certificateBaseURI
+    ) internal pure returns (bytes memory) {
+        bytes memory bytecode = type(OwlearnCourse).creationCode;
+        return
+            abi.encodePacked(
+                bytecode,
+                (
+                    courseName,
+                    courseSymbol,
+                    courseCreator,
+                    courseURI,
+                    courseNFTURIs,
+                    certificateBaseURI
+                )
+            );
     }
 }
