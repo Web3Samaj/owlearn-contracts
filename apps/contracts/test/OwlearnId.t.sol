68781737cd4569a40fefcd93e1dd3f549755921b
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-07-31 14:41:51 +0200

    test: owlearnId test completed

--- apps/contracts/test/OwlearnId.t.sol
+++ apps/contracts/test/OwlearnId.t.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/src/Test.sol";
+import "forge-std/Test.sol";
+// import "forge-std/Console.sol";
 
 import "../src/OwlearnId.sol";
 
@@ -21,5 +22,21 @@ contract OwnlearnIdScript is Test {
         owlearnId.register{value: amount}("Dhruv");
 
         assertEq(owlearnId.getNameRecordFromAddress(alice), "Dhruv");
+        OwlearnId.Record memory record = owlearnId.getNameRecord("Dhruv");
+
+        assertEq(owlearnId.ownerOf(record.tokenId), alice);
+    }
+
+    function testNFTMetadata() public {
+        address alice = address(0x1);
+        startHoax(alice, 100e18);
+
+        uint amount = owlearnId.price("Dhruv");
+
+        owlearnId.register{value: amount}("Dhruv");
+        OwlearnId.Record memory record = owlearnId.getNameRecord("Dhruv");
+        string memory metadata = owlearnId.tokenURI(record.tokenId);
+
+        console.log(metadata);
     }
 }
