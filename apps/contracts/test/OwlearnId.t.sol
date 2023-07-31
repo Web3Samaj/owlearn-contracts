2a10e096e91d715e5c0b6097b8571451c831560e
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-07-31 14:19:38 +0200

    test: OwlearnID test added

---
+++ apps/contracts/test/OwlearnId.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/src/Test.sol";
+
+import "../src/OwlearnId.sol";
+
+contract OwnlearnIdScript is Test {
+    OwlearnId public owlearnId;
+
+    function setUp() public {
+        owlearnId = new OwlearnId("owl");
+    }
+
+    function testRegister() public {
+        address alice = address(0x1);
+        startHoax(alice, 100e18);
+
+        uint amount = owlearnId.price("Dhruv");
+
+        owlearnId.register{value: amount}("Dhruv");
+
+        assertEq(owlearnId.getNameRecordFromAddress(alice), "Dhruv");
+    }
+}
