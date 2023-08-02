466887ac579fb973269b8d402de3fc91c0bc8e5e
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-08-02 17:47:14 +0530

    fix: removed commented code

--- apps/contracts/src/Factory/CourseFactory.sol
+++ apps/contracts/src/Factory/CourseFactory.sol
@@ -63,14 +63,6 @@ contract OwlearnCourseFactory is OwnableUpgradeable, CourseFactoryStorage {
         string[] memory courseNFTURIs,
         string memory certificateBaseURI
     ) external onlyEducatorBadgeHolder returns (address course, uint courseId) {
-        // bytes memory bytecode = _getByteCode(
-        //     courseName,
-        //     courseSymbol,
-        //     msg.sender,
-        //     courseURI,
-        //     courseNFTURIs,
-        //     certificateBaseURI
-        // );
         courseId = totalCourses;
         totalCourses += 1;
         bytes32 salt = keccak256(abi.encodePacked(courseName, courseSymbol));
@@ -101,49 +93,6 @@ contract OwlearnCourseFactory is OwnableUpgradeable, CourseFactoryStorage {
         return (address(_newCourse), courseId);
     }
 
-    // /**
-    //  * @dev get Address for the contract to be deployed
-    //  *
-    //  * @param courseName -Name of the new course to be created
-    //  * @param courseSymbol -Name of the new course to be created
-    //  * @param courseCreator  creator of the Course , who will also control the Collection
-    //  * @param courseURI  courseURI , containing any extra course Info
-    //  * @param courseNFTURIs  courseNFTURIs to be minted first, containing info about the particular resource
-    //  * @param certificateBaseURI -NFT URI , dynamic , off-chain server link , fetching progree & certificates for a Course Learner
-    //  *
-    //  * @return -  address of the new Course Contract to be deployed
-    //  */
-    // function getAddress(
-    //     string memory courseName,
-    //     string memory courseSymbol,
-    //     address courseCreator,
-    //     string memory courseURI,
-    //     string[] memory courseNFTURIs,
-    //     string memory certificateBaseURI
-    // ) public returns (address) {
-    //     bytes memory bytecode = _getByteCode(
-    //         courseName,
-    //         courseSymbol,
-    //         courseCreator,
-    //         courseURI,
-    //         courseNFTURIs,
-    //         certificateBaseURI
-    //     );
-    //     bytes32 salt = keccak256(abi.encodePacked(courseName, courseSymbol));
-
-    //     bytes32 hash = keccak256(
-    //         abi.encodePacked(
-    //             bytes1(0xff),
-    //             address(this),
-    //             salt,
-    //             keccak256(bytecode)
-    //         )
-    //     );
-
-    //     // NOTE: cast last 20 bytes of hash to address
-    //     return address(uint160(uint(hash)));
-    // }
-
     /*///////////////////////////////////////////////////////////////
                            Internal Functions
     //////////////////////////////////////////////////////////////*/
@@ -164,39 +113,4 @@ contract OwlearnCourseFactory is OwnableUpgradeable, CourseFactoryStorage {
             _newCourse := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
     }
-
-    // /**
-    //  * @dev get ByteCode for the contract to be deployed
-    //  *
-    //  * @param courseName -Name of the new course to be created
-    //  * @param courseSymbol -Name of the new course to be created
-    //  * @param courseCreator  creator of the Course , who will also control the Collection
-    //  * @param courseURI  courseURI , containing any extra course Info
-    //  * @param courseNFTURIs  courseNFTURIs to be minted first, containing info about the particular resource
-    //  * @param certificateBaseURI -NFT URI , dynamic , off-chain server link , fetching progree & certificates for a Course Learner
-    //  *
-    //  * @return byteCode of the new Contract to be deployed
-    //  */
-    // function _getByteCode(
-    //     string memory courseName,
-    //     string memory courseSymbol,
-    //     address courseCreator,
-    //     string memory courseURI,
-    //     string[] memory courseNFTURIs,
-    //     string memory certificateBaseURI
-    // ) internal pure returns (bytes memory) {
-    //     bytes memory bytecode = type(OwlearnCourse).creationCode;
-    //     return
-    //         abi.encodePacked(
-    //             bytecode,
-    //             abi.encodePacked(
-    //                 courseName,
-    //                 courseSymbol,
-    //                 courseCreator,
-    //                 courseURI,
-    //                 courseNFTURIs,
-    //                 certificateBaseURI
-    //             )s
-    //         );
-    // }
 }
