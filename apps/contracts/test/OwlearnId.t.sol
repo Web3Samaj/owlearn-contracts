5336c3d71fc09b274a7ee90a89a1fbba42116de0
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-08-14 10:27:58 +0530

    feat: made OwlearnId UUPS upgradeable

--- apps/contracts/test/OwlearnId.t.sol
+++ apps/contracts/test/OwlearnId.t.sol
@@ -5,13 +5,16 @@ import "forge-std/Test.sol";
 // import "forge-std/Console.sol";
 
 import "../src/OwlearnId/OwlearnId.sol";
+import "../src/Proxy/OwlearnIdProxy.sol";
 
 contract OwnlearnIdScript is Test {
     OwlearnId public owlearnId;
+    address public bob = address(0x2);
 
     function setUp() public {
-        owlearnId = new OwlearnId();
-        owlearnId.initialize("owl");
+        address owlearnIdAddress = address(new OwlearnId());
+        bytes memory initData = abi.encodeWithSelector(owlearnId.initialize.selector,"owl");
+        owlearnId = OwlearnId(address(new OwlearnIdProxy(owlearnIdAddress, initData)));
     }
 
     function testRegister() public {
@@ -39,5 +42,20 @@ contract OwnlearnIdScript is Test {
         string memory metadata = owlearnId.tokenURI(record.tokenId);
 
         console.log(metadata);
+    }
+
+    function testUpdradeable() public {
+        // address(this) is the owner
+        address newOwlearnId = address(new OwlearnId());
+        owlearnId.upgradeTo(newOwlearnId);
+    }
+
+    function testUpgradeFailOnNonOwner() public {
+        // bob is not the owner
+        startHoax(bob);
+        // upgrade Factory
+        address newOwlearnId = address(new OwlearnId());
+        vm.expectRevert("Ownable: caller is not the owner");
+        owlearnId.upgradeTo(newOwlearnId);
     }
 }
