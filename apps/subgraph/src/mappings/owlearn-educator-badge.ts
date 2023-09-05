7188c5d647c4df94e36efebb857018038cf202bf
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-09-05 23:39:43 +0530

    feat: Owlearn Educator badge completed

--- apps/subgraph/src/mappings/owlearn-educator-badge.ts
+++ apps/subgraph/src/mappings/owlearn-educator-badge.ts
@@ -1,168 +1,54 @@
 import {
-  AdminChanged as AdminChangedEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  BeaconUpgraded as BeaconUpgradedEvent,
   EducatorRegistered as EducatorRegisteredEvent,
-  Initialized as InitializedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
   TransferBatch as TransferBatchEvent,
   TransferSingle as TransferSingleEvent,
-  URI as URIEvent,
-  Upgraded as UpgradedEvent
-} from "../generated/OwlearnEducatorBadge/OwlearnEducatorBadge"
-import {
-  AdminChanged,
-  ApprovalForAll,
-  BeaconUpgraded,
-  EducatorRegistered,
-  Initialized,
-  OwnershipTransferred,
-  TransferBatch,
-  TransferSingle,
-  URI,
-  Upgraded
-} from "../generated/schema"
-
-export function handleAdminChanged(event: AdminChangedEvent): void {
-  let entity = new AdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
-  let entity = new BeaconUpgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.beacon = event.params.beacon
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+} from "../../generated/OwlearnEducatorBadge/OwlearnEducatorBadge";
+import { Educator, User } from "../../generated/schema";
 
 export function handleEducatorRegistered(event: EducatorRegisteredEvent): void {
-  let entity = new EducatorRegistered(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.educator = event.params.educator
-  entity.creatorId = event.params.creatorId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = new Educator(event.params.educator);
+  entity.address = event.params.educator;
+  entity.educatorId = event.params.creatorId;
+  let user = User.load(event.params.educator);
+  if (user == null) {
+    return;
+  }
+  entity.username = user?.username;
+  entity.save();
 }
 
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
+// Need to handle EducatorBadgeBurn , from transfer
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+// export function handleTransferBatch(event: TransferBatchEvent): void {
+//   let entity = new TransferBatch(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   );
+//   entity.operator = event.params.operator;
+//   entity.from = event.params.from;
+//   entity.to = event.params.to;
+//   entity.ids = event.params.ids;
+//   entity.values = event.params.values;
 
-  entity.save()
-}
+//   entity.blockNumber = event.block.number;
+//   entity.blockTimestamp = event.block.timestamp;
+//   entity.transactionHash = event.transaction.hash;
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+//   entity.save();
+// }
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+// export function handleTransferSingle(event: TransferSingleEvent): void {
+//   let entity = new TransferSingle(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   );
+//   entity.operator = event.params.operator;
+//   entity.from = event.params.from;
+//   entity.to = event.params.to;
+//   entity.OwlearnEducatorBadge_id = event.params.id;
+//   entity.value = event.params.value;
 
-  entity.save()
-}
+//   entity.blockNumber = event.block.number;
+//   entity.blockTimestamp = event.block.timestamp;
+//   entity.transactionHash = event.transaction.hash;
 
-export function handleTransferBatch(event: TransferBatchEvent): void {
-  let entity = new TransferBatch(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.ids = event.params.ids
-  entity.values = event.params.values
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTransferSingle(event: TransferSingleEvent): void {
-  let entity = new TransferSingle(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.OwlearnEducatorBadge_id = event.params.id
-  entity.value = event.params.value
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleURI(event: URIEvent): void {
-  let entity = new URI(event.transaction.hash.concatI32(event.logIndex.toI32()))
-  entity.value = event.params.value
-  entity.OwlearnEducatorBadge_id = event.params.id
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUpgraded(event: UpgradedEvent): void {
-  let entity = new Upgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+//   entity.save();
+// }
