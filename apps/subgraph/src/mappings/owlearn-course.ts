a6922469a92eea0deb11e12103950d51d06f7cca
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-09-05 23:57:57 +0530

    feat: Owlearn Course methods added

--- apps/subgraph/src/mappings/owlearn-course.ts
+++ apps/subgraph/src/mappings/owlearn-course.ts
@@ -1,133 +1,44 @@
 import {
-  AdminChanged as AdminChangedEvent,
-  BeaconUpgraded as BeaconUpgradedEvent,
   CourseInitialised as CourseInitialisedEvent,
-  Initialized as InitializedEvent,
   MintModuleDisabled as MintModuleDisabledEvent,
   MintModuleInitialised as MintModuleInitialisedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  Upgraded as UpgradedEvent
-} from "../generated/OwlearnCourse/OwlearnCourse"
+} from "../../generated/templates/OwlearnCourse/OwlearnCourse";
+import { Course, Educator } from "../../generated/schema";
 import {
-  AdminChanged,
-  BeaconUpgraded,
-  CourseInitialised,
-  Initialized,
-  MintModuleDisabled,
-  MintModuleInitialised,
-  OwnershipTransferred,
-  Upgraded
-} from "../generated/schema"
-
-export function handleAdminChanged(event: AdminChangedEvent): void {
-  let entity = new AdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
-  let entity = new BeaconUpgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.beacon = event.params.beacon
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+  OwlearnCourseCertificates,
+  OwlearnCourseResource,
+} from "../../generated/templates";
 
 export function handleCourseInitialised(event: CourseInitialisedEvent): void {
-  let entity = new CourseInitialised(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.course = event.params.course
-  entity.resource = event.params.resource
-  entity.certificates = event.params.certificates
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = Course.load(event.params.course);
+  if (entity == null) {
+    return;
+  }
+  entity.resourceAddress = event.params.resource;
+  entity.certificateAddress = event.params.certificates;
+  entity.certificate = event.params.certificates;
+  OwlearnCourseCertificates.create(event.params.resource);
+  OwlearnCourseResource.create(event.params.certificates);
+  entity.save();
 }
 
 export function handleMintModuleDisabled(event: MintModuleDisabledEvent): void {
-  let entity = new MintModuleDisabled(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.course = event.params.course
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = Course.load(event.params.course);
+  if (entity == null) {
+    return;
+  }
+  entity.mintModule = null;
+  entity.save();
 }
 
 export function handleMintModuleInitialised(
   event: MintModuleInitialisedEvent
 ): void {
-  let entity = new MintModuleInitialised(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.course = event.params.course
-  entity.moduleAddress = event.params.moduleAddress
+  let entity = Course.load(event.params.course);
+  if (entity == null) {
+    return;
+  }
+  entity.mintModule = event.params.moduleAddress;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUpgraded(event: UpgradedEvent): void {
-  let entity = new Upgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  entity.save();
 }
