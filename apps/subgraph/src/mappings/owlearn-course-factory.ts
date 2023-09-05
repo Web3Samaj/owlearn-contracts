9dde282df0149a4d7e2bd9c63fb341dc50b6c303
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-09-05 23:52:41 +0530

    feat: Owlearn Course Factory method added

--- apps/subgraph/src/mappings/owlearn-course-factory.ts
+++ apps/subgraph/src/mappings/owlearn-course-factory.ts
@@ -1,106 +1,26 @@
-import {
-  AdminChanged as AdminChangedEvent,
-  BeaconUpgraded as BeaconUpgradedEvent,
-  CourseCreated as CourseCreatedEvent,
-  Initialized as InitializedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  Upgraded as UpgradedEvent
-} from "../generated/OwlearnCourseFactory/OwlearnCourseFactory"
-import {
-  AdminChanged,
-  BeaconUpgraded,
-  CourseCreated,
-  Initialized,
-  OwnershipTransferred,
-  Upgraded
-} from "../generated/schema"
-
-export function handleAdminChanged(event: AdminChangedEvent): void {
-  let entity = new AdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
-  let entity = new BeaconUpgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.beacon = event.params.beacon
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+import { CourseCreated as CourseCreatedEvent } from "../../generated/OwlearnCourseFactory/OwlearnCourseFactory";
+import { Course, Educator } from "../../generated/schema";
+import { OwlearnCourse } from "../../generated/templates";
 
 export function handleCourseCreated(event: CourseCreatedEvent): void {
-  let entity = new CourseCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.creatorId = event.params.creatorId
-  entity.courseId = event.params.courseId
-  entity.courseAddress = event.params.courseAddress
-  entity.courseName = event.params.courseName
-  entity.courseSymbol = event.params.courseSymbol
-  entity.creator = event.params.creator
-  entity.courseURI = event.params.courseURI
-  entity.courseNFTURIs = event.params.courseNFTURIs
-  entity.certificateBaseURI = event.params.certificateBaseURI
+  let entity = new Course(event.params.courseAddress);
+  entity.creatorId = event.params.creatorId;
+  entity.courseId = event.params.courseId;
+  entity.address = event.params.courseAddress;
+  entity.name = event.params.courseName;
+  entity.symbol = event.params.courseSymbol;
+  entity.courseURI = event.params.courseURI;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  // assign the educator
+  let creator = event.params.creator;
+  let educator = Educator.load(creator);
+  if (educator == null) {
+    return;
+  }
+  entity.educator = educator.id;
 
-  entity.save()
-}
+  // Add New Course from the templates\
+  OwlearnCourse.create(event.params.courseAddress);
 
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUpgraded(event: UpgradedEvent): void {
-  let entity = new Upgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  entity.save();
 }
